37bf87e619314b32546ab9d58125d347b02f1da2
Ricardo Rodrigo de Oliveira <ricardorodrigo.oliveira@gmail.com>
2023-01-17 15:12:50 -0300

    Updates

---
+++ precache-manifest.522d50150ad10c054b7a9d3dfe1edf2a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "762797202228fe0912b3",
+    "url": "/mypage/static/css/main.b561c22c.chunk.css"
+  },
+  {
+    "revision": "762797202228fe0912b3",
+    "url": "/mypage/static/js/main.76279720.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/mypage/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "e6b2a3b6c9e3dc2f823b",
+    "url": "/mypage/static/js/2.e6b2a3b6.chunk.js"
+  },
+  {
+    "revision": "4dcbd3e1c5b908c8e5a0",
+    "url": "/mypage/static/js/runtime~main.4dcbd3e1.js"
+  },
+  {
+    "revision": "07780903a26e2cbdb3903989fe3cd37e",
+    "url": "/mypage/static/media/avatar1.07780903.jpg"
+  },
+  {
+    "revision": "5a9f7e4f6a5dc10d51650c8655541897",
+    "url": "/mypage/static/media/avatar3.5a9f7e4f.jpg"
+  },
+  {
+    "revision": "7ff7c382fa5920ac2e77c1dae5f9dbc9",
+    "url": "/mypage/static/media/img-1.7ff7c382.jpg"
+  },
+  {
+    "revision": "b5b4de61130285f41341f0c0e4ad1be9",
+    "url": "/mypage/static/media/img-2.b5b4de61.jpg"
+  },
+  {
+    "revision": "e4e815d5b2f42ae93731950165e802cc",
+    "url": "/mypage/static/media/img-3.e4e815d5.jpg"
+  },
+  {
+    "revision": "e289e6f948365df1efc1a37e430d51ba",
+    "url": "/mypage/static/media/img-4.e289e6f9.jpg"
+  },
+  {
+    "revision": "6029339ede274d97931faabd92c4c90d",
+    "url": "/mypage/static/media/img-5.6029339e.jpg"
+  },
+  {
+    "revision": "b03ae3f7bab61ebe50ca4df7fe4b7ecd",
+    "url": "/mypage/static/media/img-6.b03ae3f7.jpg"
+  },
+  {
+    "revision": "9dc9f7c2d13a44170c2a42cc2323eb7d",
+    "url": "/mypage/static/media/img-7.9dc9f7c2.jpg"
+  },
+  {
+    "revision": "ac5f141e22b14904a9b6630b431d9908",
+    "url": "/mypage/static/media/img-8.ac5f141e.jpg"
+  },
+  {
+    "revision": "dc8d10d4e653e5ce1340bc869524bcf7",
+    "url": "/mypage/static/media/img-9.dc8d10d4.jpg"
+  },
+  {
+    "revision": "405bb92105677b5af94fdd8a5ea65be0",
+    "url": "/mypage/static/media/header.405bb921.jpg"
+  },
+  {
+    "revision": "ad82481a70701b57d0e848c3dc0c57e4",
+    "url": "/mypage/index.html"
+  }
+];
